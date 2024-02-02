70dffa566f747e38161f657e60da767bcc89e83b
juancruzoc <ocamposjuanc@gmail.com>
2024-02-02 14:39:14 -0300

    Now the reports correspond to the viewed short link.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,6 +2,6 @@ class ReportsController < ApplicationController
   def show
     @short_link = ShortLink.find(params[:id])
     @q = Report.ransack(params[:q])
-    @reports = @q.result(distinct: true)
+    @reports = @q.result(distinct: true).where(short_link_id: @short_link.id)
   end
 end
