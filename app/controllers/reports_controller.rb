2a1d1416ac0cd7fb9c1d28ff44b37f7990d5e238
juancruzoc <ocamposjuanc@gmail.com>
2024-02-03 11:51:28 -0300

    Added validation for logged user.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,14 @@
 class ReportsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @short_link = ShortLink.find(params[:id])
-    @q = Report.ransack(params[:q])
-    @reports = @q.result(distinct: true).where(short_link_id: @short_link.id)
+    if @short_link.user_id != current_user.id
+      not_found
+    else
+      @q = Report.ransack(params[:q])
+      @reports = @q.result(distinct: true).where(short_link_id: @short_link.id)
+    end
   end
+
 end
