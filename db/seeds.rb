7d2726e12ea61058c8d4ca865fdfaca96eca8f49
juancruzoc <ocamposjuanc@gmail.com>
2024-02-08 17:19:27 -0300

    Randomize the reports creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'ipaddr'
+
 User.destroy_all
 ShortLink.destroy_all
 Report.destroy_all
@@ -14,121 +16,80 @@ Report.destroy_all
 @empty_user.password_confirmation = '123123'
 @empty_user.save!
 
-ShortLink.destroy_all
+$now = Time.now
+$a_day_ago = $now - 86400 # now - 60 * 60 * 24
+$user_agent_list = ["Mozilla/5.0", "Chrome/121.0.0.0", "CriOS/121.0.6167.171", "Safari/537.36"]
 
-@report_link = ShortLink.new({ # Reports link
+def time_rand from = 0.0, to = Time.now
+  Time.at(from + rand * (to.to_f - from.to_f))
+end
+
+def generate_reports short_link_id
+  rand(5..15).times do
+    Report.create!([{
+      short_link_id: short_link_id,
+      ip: IPAddr.new(rand(2**32),Socket::AF_INET),
+      date: (time_rand Time.local(2023, 12, 1), Time.now),
+      hour: rand($a_day_ago..$now),
+      user_agent: $user_agent_list.sample,
+    }])
+  end
+end
+
+@default_link = ShortLink.new({
   user_id: @test_user.id,
   url: "http://facebook.com",
   short_url: "4f7c06c",
   password: ""
 })
-@report_link.save
+@default_link.save
+generate_reports @default_link.id
 
-ShortLink.create!([{ # Password protected link
-    user_id: @test_user.id,
-    url: "http://x.com",
-    short_url: "1b3b988",
-    password: "123123"
-  },
-  { # Link with setted expiration date
-    user_id: @test_user.id,
-    url: "http://medium.com",
-    short_url: "c371990",
-    password: "",
-    expiration_date: Date.new(2025,01,31),
-  },
-  { # Usages capped link
-    user_id: @test_user.id,
-    url: "http://youtube.com",
-    short_url: "64aa817",
-    password: "",
-    usages: "50"
-  },
-  { # 0 Usages left link
-    user_id: @test_user.id,
-    url: "http://linkedin.com",
-    short_url: "e5373d5",
-    password: "",
-    usages: "0"
-  }]
-)
+@password_protected_link = ShortLink.new({
+  user_id: @test_user.id,
+  url: "http://x.com",
+  short_url: "1b3b988",
+  password: "123123"
+})
+@password_protected_link.save
+generate_reports @password_protected_link.id
 
-ShortLink.new({ # Expired link
+@not_expired_link = ShortLink.new({
+  user_id: @test_user.id,
+  url: "http://medium.com",
+  short_url: "c371990",
+  password: "",
+  expiration_date: Date.new(2025,01,31),
+})
+@not_expired_link.save
+generate_reports @not_expired_link.id
+
+@usages_capped_link = ShortLink.new({
+  user_id: @test_user.id,
+  url: "http://youtube.com",
+  short_url: "64aa817",
+  password: "",
+  usages: "50"
+})
+@usages_capped_link.save
+generate_reports @usages_capped_link.id
+
+@zero_usages_link = ShortLink.new({
+  user_id: @test_user.id,
+  url: "http://linkedin.com",
+  short_url: "e5373d5",
+  password: "",
+  usages: "0"
+})
+@zero_usages_link.save
+generate_reports @zero_usages_link.id
+
+@expired_link = ShortLink.new({ # Expired link
     user_id: @test_user.id,
     url: "http://instagram.com",
     short_url: "c3c40c8",
     password: "",
     expiration_date: Date.new(2023,12,31),
-  }).save(validate: false)
-
-Report.create!([{
-  short_link_id: @report_link.id,
-  ip: "238.245.60.165",
-  date: Date.new(2024, 1, 15),
-  hour: Time.now.midnight,
-  user_agent: "Mozilla/5.0"
-  },
-  {
-    short_link_id: @report_link.id,
-    ip: "49.31.210.164",
-    date: Date.new(2024, 1, 16),
-    hour: Time.now.midday,
-    user_agent: "Mozilla/5.0"
-  },
-  {
-    short_link_id: @report_link.id,
-    ip: "10.111.2.94",
-    date: Date.new(2024, 1, 16),
-    hour: Time.now.midnight,
-    user_agent: "Chromium"
-  },
-  {
-    short_link_id: @report_link.id,
-    ip: "173.156.71.29",
-    date: Date.new(2024, 1, 17),
-    hour: Time.now.midday,
-    user_agent: "Chrome"
-  },
-  {
-    short_link_id: @report_link.id,
-    ip: "173.156.71.29",
-    date: Date.new(2024, 1, 20),
-    hour: Time.now.midnight,
-    user_agent: "Chrome"
-  },
-  {
-    short_link_id: @report_link.id,
-    ip: "227.10.252.218",
-    date: Date.new(2024, 1, 25),
-    hour: Time.now.midday,
-    user_agent: "Mozilla/5.0"
-    },
-    {
-      short_link_id: @report_link.id,
-      ip: "227.122.8.98",
-      date: Date.new(2024, 2, 1),
-      hour: Time.now.midnight,
-      user_agent: "Mozilla/5.0"
-    },
-    {
-      short_link_id: @report_link.id,
-      ip: "141.211.225.179",
-      date: Date.new(2024, 2, 1),
-      hour: Time.now.midday,
-      user_agent: "Chromium"
-    },
-    {
-      short_link_id: @report_link.id,
-      ip: "90.181.161.171",
-      date: Date.new(2024, 2, 1),
-      hour: Time.now.midnight,
-      user_agent: "Chrome"
-    },
-    {
-      short_link_id: @report_link.id,
-      ip: "70.115.217.237",
-      date: Date.new(2024, 2, 1),
-      hour: Time.now.midday,
-      user_agent: "Chrome"
-    },]
-)
+})
+@expired_link.save(validate: false)
+generate_reports @expired_link.id
